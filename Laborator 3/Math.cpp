71f8970ffb9c24c21a67d48942a2cf09fb25d8f4
loriansandu <loriansandu@gmail.com>
2021-03-03 12:51:42 +0200

    Am adaugat functiile

--- Laborator 3/Math.cpp
+++ Laborator 3/Math.cpp
@@ -1,4 +1,30 @@
 //
 // Created by Lorian-Andrei Sandu on 03.03.2021.
 //
+#include "Math.h"
+int Math::Add(int x, int y) {
+    return x+y;
+
+}
+int Math::Add(int x, int y, int z) {
+    return x+y+z;
+}
+int Math::Add(double x, double y) {
+    return x+y;
+}
+int Math::Add(double x, double y, double z) {
+    return x+y+z;
+}
+int Math::Mul(int x, int y) {
+    return x*y;
+}
+int Math::Mul(int x, int y, int z) {
+    return x*y*z;
+}
+int Math::Mul(double x, double y) {
+    return x*y;
+}
+int Math::Mul(double x, double y, double z) {
+    return x*y*z;
+}
 
